4719c91a02c86cd535e072c5132078a50deee92c
Byron Formwalt <byron@fastmail.net>
2014-04-12 11:02:33 -0600

    Cleaned up code.

--- question_006.rb
+++ question_006.rb
@@ -4,23 +4,37 @@
 
 #ref: http://www.careercup.com/question?id=4981417205301248
 
-x = 3
-
-
-s = x.to_s(2)
-puts "x = #{x} ==> #{s}b"
-# Find the right most "1" that can shift left one position onto a "0"
-a = s.reverse.split("") << "0"
-j = 0
-i = a.find_index do |v|
-   j += 1
-   v == "1" && a[j] == "0"
+module DigitMagic
+  def magic_shift
+    s = self.to_s(2)
+    # Find the right most "1" that can shift left one position onto a "0"
+    a = s.reverse.split("") << "0"
+    j = 0
+    i = a.find_index do |v|
+      j += 1
+      v == "1" && a[j] == "0"
+    end
+    a[i] = "0"
+    a[i+1] = "1"
+    a.pop if a[-1] == "0"
+    s2 = a.join.reverse
+    x2 = s2.to_i(2)
+    puts "x = #{self} ==> #{s}b; x = #{x2} ==> #{s2}b\n"
+  end
 end
-a[i] = "0"
-a[i+1] = "1"
-s2 = a.join.reverse
-x2 = s2.to_i(2)
-puts "The next integer with the same number of ones in its " +
-      "binary representation is x = #{x2} ==> #{s2}b"
+
+class Fixnum
+  include DigitMagic
+end
+
+class Bignum
+  include DigitMagic
+end
+
+x = [380,3,24,513]
+x.each{|v| v.magic_shift}
+
+
+        
       
 

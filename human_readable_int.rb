29516a4bea3b5a4e30975c3774414fc86898cce5
Byron Formwalt <byron@fastmail.net>
2014-04-11 21:58:58 -0600

    .

--- human_readable_int.rb
+++ human_readable_int.rb
@@ -3,9 +3,9 @@
 print "Enter a non-negative integer, then press <ENTER>: "
 x = gets.to_i
 
-s = x.to_s.reverse.bytes.each_slice(3).collect do |t| 
-  "," + t.pack("c*")
-end
-s = s.join.reverse.chop
+s = x.to_s.reverse.split("").inject("") do |t,c| 
+  t + ((t.length + 1) % 4 == 0 ? ",": "") + c
+end.reverse
 print "\t#{x} -> \"#{s}\"\n"
+
 

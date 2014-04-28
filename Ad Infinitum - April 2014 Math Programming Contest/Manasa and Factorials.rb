d4b6bafb193fdd227198e17cf25df717b5f3106a
Byron Formwalt <byron@fastmail.net>
2014-04-27 21:29:07 -0600

    Modified ruby solution to use a binary search.

--- Ad Infinitum - April 2014 Math Programming Contest/Manasa and Factorials.rb
+++ Ad Infinitum - April 2014 Math Programming Contest/Manasa and Factorials.rb
@@ -4,7 +4,7 @@ include Math
 
 
 $stdin = StringIO.new(
-"4
+"23
 5
 246
 2
@@ -26,42 +26,20 @@ $stdin = StringIO.new(
 247
 248
 249
-250")
-
-$answers = [25,
-995,
-10,
-15,
-20,
-25,
-25,
-30,
-95,
-100,
-100,
-105,
-620,
-625,
-625,
-625,
-625,
-995,
-1000,
-1000,
-1000,
-1005]
+250
+10000000000000000")
 
 def greatest_mult(m)
   # Returns the greatest positive integer, q, less than or equal to m such that there exist an integer, r > 1 that satisfies q.mod(5**r) == 0.
   return nil if m < 5**2 || !m.kind_of?(Fixnum)
   r_max = (log(m)/log(5)).floor
-  q = 2.upto(r_max).collect{|r| q0 = 5**r; (m/q0)*q0}.max
+  q = 2.upto(r_max).collect{|r| q0 = 5**r (m/q0)*q0}.max
 end
 
 def count_roots(m)
   # For all positive integers i <= m, returns a hash containing the number of occurrences of each multiple which has root r of 5.
   r_counts = Hash.new(0)
-  return if m < 25
+  return r_counts if m < 25
   r_max = (log(m)/log(5)).floor
   2.upto(r_max) do |r|
     r_counts[r] = m/5**r
@@ -78,43 +56,40 @@ def count_roots(m)
 end
 
 def count_extras(r_counts)
-  r_counts.collect do |r,count|
+  n_extra = r_counts.collect do |r,count|
     (r-1)*count
   end.inject(:+)
+  n_extra ||= 0
 end
 
-def compute_m(n)
-  m = 5*n
-  r_counts = count_roots(m)
-  return m if !r_counts
-  
-  n_extra = count_extras(r_counts)
-  n_actual = n + n_extra  
-  
-  m_proposed = m
-  while n < n_actual do
-    m = m_proposed
-    m_proposed = greatest_mult(m - 1)
-    if !m_proposed
-      m_proposed = m
-      break
-    end
-    n_extra = count_extras(count_roots(m_proposed))
-    n_actual = m_proposed/5 + n_extra
+def bsearch_for_m(n,m_max,m_min,level = 0)
+  # Start by analyzing the midpoint between m_min and m_max.
+     
+  if m_max - m_min == 5 
+    # Terminal search condition.
+    n_actual_min = m_min/5 + count_extras(count_roots(m_min))
+    n_actual_max = m_max/5 + count_extras(count_roots(m_max))
+    m_proposed = n_actual_min >= n ? m_min : m_max
+    return m_proposed
   end
-  m = m_proposed
-  while n > n_actual do
-    m_proposed += 5
-    n_extra = count_extras(count_roots(m_proposed))
-    n_actual = m_proposed/5 + n_extra    
+  m_proposed = ((m_max + m_min)/10)*5
+  n_extra = count_extras(count_roots(m_proposed))
+  n_actual = m_proposed/5 + n_extra
+    
+  if n_actual > n
+    return bsearch_for_m(n, m_proposed, m_min, level + 1)
+  elsif n_actual < n
+    return bsearch_for_m(n, m_max, m_proposed, level + 1)
+  else
+    # Nailed it.
+    return m_proposed
   end
-  m = m_proposed
 end
-
+    
+    
 nt = $stdin.gets.to_i
 a = $stdin.each_line.collect{|l| l.to_i}
 a.each_with_index do |n,i|
-  m = compute_m(n)
+  m = bsearch_for_m(n,5*n,0)
   puts m
-  raise "Wrong answer." if m != $answers[i]
 end
